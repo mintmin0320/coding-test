85f1aa165821fe256eab0920738049745f7404ec
박하민 <114549939+mintmin0320@users.noreply.github.com>
2023-02-28 02:35:12 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,3 @@
 function solution(arr) {
-    var answer = 0;
-    for(var i = 0; i < arr.length; i++) {
-        answer += arr[i];
-    }
-    return answer / arr.length;
+  return arr.reduce((a, b) => a + b) / arr.length
 }
