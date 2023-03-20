97d2b135ffd9d9b3c713b45710bcf01be891756a
박하민 <114549939+mintmin0320@users.noreply.github.com>
2023-03-20 16:16:17 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,32 +1,9 @@
 function solution(a, b) {
     var month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
     var sum = b;
-    var answer = "";
     for (var i = 0; i < a - 1; i++) {
         sum += month[i];
      }
-    switch (sum % 7) {
-        case 0:
-            answer = "THU";
-            break;
-        case 1:
-            answer = "FRI";
-            break;
-        case 2:
-            answer = "SAT";
-            break;
-        case 3:
-            answer = "SUN";
-            break;
-        case 4:
-            answer = "MON";
-            break;
-        case 5:
-            answer = "TUE";
-            break;
-        case 6:
-            answer = "WED";
-            break;
-      }
-      return answer;
+    return week[sum % 7];
 }
