d223901bd7a2e394570c47433099fdfed08684d3
박하민 <114549939+mintmin0320@users.noreply.github.com>
2023-05-01 16:44:16 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+      for (v of goal) {
+    if (cards1[0] === v) {
+      cards1.shift();
+    } else if (cards2[0] === v) {
+      cards2.shift();
+    } else {
+      return "No";
+    }
+  }
+  return "Yes";
+}
