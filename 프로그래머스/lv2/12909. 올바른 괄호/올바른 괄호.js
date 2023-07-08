e2d3fee3ba200e6d07dd80260d625d0c8a94e809
박하민 <mintmin0320@gmail.com>
2023-07-09 02:04:05 +0900

    [level 2] Title: 올바른 괄호, Time: 5.82 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,14 @@
 function solution(s){
-    let arr = [...s];
-    let cnt = 0;
+    let stack = [];
     
-    if(arr === ')' || arr[arr.length-1] === '(') return false;
+    if(s[0] === ')' || s[s.length-1] === '(') return false; 
     
-    for(const x of arr){
-        if(x === '(') {
-            cnt += 1;
-        }else {
-            cnt -= 1;
+    for(const v of s){
+        if(v === '(') stack.push(v);
+        else {
+            stack.pop();
         }
-        
-        if(cnt < 0) return false;
     }
-    return !cnt;
+    if(stack.length > 0) return false;
+    return true;
 }
