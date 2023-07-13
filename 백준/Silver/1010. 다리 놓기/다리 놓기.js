6e0ee7f5b58d6040e27799d5f05de76b0196facb
박하민 <mintmin0320@gmail.com>
2023-07-13 11:30:43 +0900

    [Silver V] Title: 다리 놓기, Time: 236 ms, Memory: 10960 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  };
+
+  return n * factorial(n - 1);
+};
+
+for (let i = 1; i <= input[0]; i++) {
+  let arr = input[i].split(' ').map(Number);
+  let left = arr[0];
+  let right = arr[1];
+
+  if (left === right) {
+    console.log(1);
+    continue;
+  };
+
+  console.log(parseInt(factorial(right) / (factorial(left) * factorial(right - left)) + 0.5));
+};
